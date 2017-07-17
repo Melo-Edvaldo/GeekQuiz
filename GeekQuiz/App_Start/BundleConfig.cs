00cfea6fda4d7b4f860d02bf2bfb04c285018aa5
Edvaldo de Oliveira Melo <edvaldomelo@byui.edu>
2017-07-17 20:15:07 -0300

    Updating

--- GeekQuiz/App_Start/BundleConfig.cs
+++ GeekQuiz/App_Start/BundleConfig.cs
@@ -26,6 +26,11 @@ namespace GeekQuiz
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/site.css",
+                        "~/Content/Flip.css"));
         }
     }
 }
